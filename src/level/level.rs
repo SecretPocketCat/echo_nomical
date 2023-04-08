4f106d85bedc8f7a0827aae13d086fa8eccec63c
SecretPocketCat <jiridexterhanc@gmail.com>
2023-04-08 07:36:55 +0200

    add rand crate

--- src/level/level.rs
+++ src/level/level.rs
@@ -1,5 +1,8 @@
+use std::f32::consts::PI;
+
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
+use rand::*;
 
 #[derive(Resource, Deref)]
 pub struct LevelSize(pub Vec2);
@@ -11,6 +14,8 @@ pub struct LevelEntry;
 pub struct LevelExit;
 
 pub(super) fn setup_test_lvl(mut cmd: Commands) {
+    let mut rng = thread_rng();
+
     for polyline in [
         // walls
         vec![
@@ -28,19 +33,26 @@ pub(super) fn setup_test_lvl(mut cmd: Commands) {
         vec![(250., 200.), (400., 230.), (380., 90.)],
         vec![(300., 280.), (220., 300.), (-200., -210.), (100., -180.)],
     ] {
-        let mut vertices: Vec<_> = polyline.iter().map(|(x, y)| Vec2::new(*x, *y)).collect();
-        vertices.push(polyline[0].into());
+        let mut vertices: Vec<_> = polyline
+            .iter()
+            .map(|(x, y)| {
+                Vec2::new(
+                    *x + rng.gen_range(-50.0..50.),
+                    *y + rng.gen_range(-50.0..50.),
+                )
+            })
+            .collect();
+        vertices.push(vertices[0]);
         cmd.spawn(Collider::polyline(vertices, None));
     }
 
     cmd.spawn(TransformBundle::from_transform(Transform::from_xyz(
-        280., 20., 0.,
+        330., 20., 0.,
     )))
     .insert(LevelEntry);
 
     cmd.spawn(TransformBundle::from_transform(Transform::from_xyz(
-        280., 20., 0.,
-        // -325., 260., 0.,
+        -325., 260., 0.,
     )))
     .insert(Collider::cuboid(25., 25.))
     .insert(Sensor)
